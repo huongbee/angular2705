80440e66f1dd6bb529e236734fe58f5715cea8a5
Huong <huongnguyenak96@gmail.com>
2019-07-24 21:29:33 +0700

    binding

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,8 +3,31 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  // template: `
+  //   <h1>Hello</h1>
+  //   <h1>Hello</h1>
+  // `,
+  // styleUrls: ['./app.component.css']
+  styles: [
+    `
+      h1{
+        color:red
+      }
+    `,
+    ``
+  ]
 })
 export class AppComponent {
   title = 'angular2705';
+  name = 'Angular';
 }
+
+/**
+ * B1: tao component: ng g c name
+ * B2: khai bao:
+ *      import ở app.module.ts
+ *      thêm: FirstComponent vào declarations
+ * B3: sử dụng
+ *
+ * index.html -> root:AppComponent -> other cpn
+ */
