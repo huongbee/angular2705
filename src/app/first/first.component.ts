5a27f9b5699b6de9da18e3b4565954df2a99f3ac
Huong <huongnguyenak96@gmail.com>
2019-07-26 21:25:23 +0700

    ngModel

--- src/app/first/first.component.ts
+++ src/app/first/first.component.ts
@@ -6,10 +6,17 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./first.component.css']
 })
 export class FirstComponent implements OnInit {
-
+  name: string;
+  age: number;
   constructor() { }
 
   ngOnInit() {
+  }
+  getDataInput(input) {
+    this.name = input.value;
+  }
+  getData(age) {
+    this.age = isNaN(age.value) ? 0 : age.value;
   }
 
 }
