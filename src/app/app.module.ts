bbe21ae7defc9b3642d73a7aee24c2301b45edeb
Huong <huongnguyenak96@gmail.com>
2019-07-29 20:52:23 +0700

    signup

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,12 +6,14 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { FirstComponent } from './first/first.component';
 import { SecondComponent } from './second/second.component';
+import { SignupComponent } from './signup/signup.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     FirstComponent,
-    SecondComponent
+    SecondComponent,
+    SignupComponent
   ],
   imports: [
     BrowserModule,
